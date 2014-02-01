92608ce57d70ae8128d61a995815dcfd9e9808db
Mike <mchau@iwu.edu>
2014-02-01 05:02:10 -0600

    kill any xbmc processes except xbmc-manager

--- xbmc-manager.sh
+++ xbmc-manager.sh
@@ -15,7 +15,7 @@ fi
 touch manager.log
 
 if [ $(pidof -x xbmc.bin) > /dev/null ]; then
-  ps x | grep xbmc.bin | grep -v grep | awk '{print "kill -15 " $1}' | sh
+  ps x | grep xbmc | grep -v grep | grep -v *.sh | awk '{print "kill -15 " $1}' | sh
   echo "[$(date)]: Killed XBMC" >> manager.log
   sleep 30
 fi
